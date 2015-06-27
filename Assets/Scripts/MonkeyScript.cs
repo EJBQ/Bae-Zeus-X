86ef1b23dd1f57eb627ede95e8e820b2e86891c4
EJBQ <EJBQuinn@gmail.com>
2015-06-27 01:11:00 -0700

    Solving null target issues

--- Assets/Scripts/MonkeyScript.cs
+++ Assets/Scripts/MonkeyScript.cs
@@ -7,13 +7,15 @@ public class MonkeyScript : MonoBehaviour {
 	public Transform player;
 
 	void FixedUpdate() {
-		float z = Mathf.Atan2 ((player.transform.position.y - transform.position.y), (player.transform.position.x - 
-			transform.position.x)) *
-			Mathf.Rad2Deg - 90;
+		if (player != null) {
+			float z = Mathf.Atan2 ((player.transform.position.y - transform.position.y), (player.transform.position.x - 
+				transform.position.x)) *
+				Mathf.Rad2Deg - 90;
 
-		transform.eulerAngles = new Vector3 (0, 0, z - 90);
+			transform.eulerAngles = new Vector3 (0, 0, z - 90);
 
-		this.GetComponent<Rigidbody2D>().AddForce(-1 * gameObject.transform.right * speed);
+			this.GetComponent<Rigidbody2D> ().AddForce (-1 * gameObject.transform.right * speed);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
