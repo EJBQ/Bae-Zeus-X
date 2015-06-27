3d13cca8534557034551b37ea3d35f80e0309afa
Nicholas Goh <nicgoh@amazon.com>
2015-06-27 10:02:13 -0700

    iPhone collisions may work right now.

---
+++ Assets/Scripts/HitStars.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitStars : MonoBehaviour {
+
+	void Start() {
+		Invoke("Die", 1f);
+	}
+	
+	void Die() {
+		Destroy(gameObject);
+	}
+}	
--- Assets/Scripts/iPhoneBehaviour.cs
+++ Assets/Scripts/iPhoneBehaviour.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class iPhoneBehaviour : MonoBehaviour {
 
+	public GameObject blueStars;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,7 +15,21 @@ public class iPhoneBehaviour : MonoBehaviour {
 		}
 	}
 
-	public void destroyiPhoneOffScreen() {
+	void destroyiPhoneOffScreen() {
 		Destroy (this.gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+
+		if (collider.gameObject.CompareTag("Player")) {
+			Instantiate(blueStars, this.gameObject.transform.position, Quaternion.identity);
+			collider.gameObject.GetComponent<CharacterBehavior>().loseHealth();
+			Invoke ("die", 1.2f);
+		}
+
+	}
+
+	void die() {
+		Destroy (gameObject);
 	}
 }
