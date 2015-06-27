ffc54b9a66817f349e4c8327ab32d9467682d9d6
Nicholas Goh <nicgoh@amazon.com>
2015-06-26 23:47:51 -0700

    Monkey Cube is now valid (Sorry if I break things)

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+	GameManager gameManager;
+	
+	
+	// Use this for initialization
+	void Start () {
+//		gameManager = GameObject.Find("gameManager").GetComponent<GameManager>();
+//		gameManager.gameStart ();
+	}
+}
---
+++ Assets/Scripts/MonkeyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyScript : MonoBehaviour {
+
+	public float speed;
+	public Transform player;
+
+	void FixedUpdate() {
+		float z = Mathf.Atan2 ((player.transform.position.y - transform.position.y), (player.transform.position.x - 
+			transform.position.x)) *
+			Mathf.Rad2Deg - 90;
+
+		transform.eulerAngles = new Vector3 (0, 0, z);
+
+		this.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		// I should probably error check this but fuck it
+		Destroy (other.gameObject);
+		// TODO: Make you lose.
+	}
+}
